adc576ea85c2521fbad5216041e99f9a2724aaeb
Arnaud293 <arnaudlt293@gmail.com>
2022-10-31 15:45:14 +0100

    create static data for developement

---
+++ src/config/data.js
@@ -0,0 +1,26 @@
+const initialData = {
+    tasks: {
+        "task-1" : {id: "task-1", content: "Une random tâche"},
+        "task-2" : {id: "task-2", content: "Autre random tâche"},
+        "task-3" : {id: "task-3", content: "Encore une random tâche"},
+        "task-4" : {id: "task-4", content: "Cette tâche est random"},
+    },
+    columns: {
+        "column-1" : {
+            id : "column-1",
+            title: "Tâches à faire",
+            taskIds: ["task-1", "task-2", "task-3", "task-4"],
+        },
+        "column-2" : {
+            id : "column-1",
+            title: "Tâches à faire",
+            taskIds: [],
+        },
+        "column-3" : {
+            id : "column-1",
+            title: "Tâches à faire",
+            taskIds: [],
+        },
+    },
+    columnsOrder : ["column-1", "column-2", "column-3"],
+}
