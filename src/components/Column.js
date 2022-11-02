34a6cdf3cbfc9b7f625ba74ffe88f993ceabc62f
Arnaud293 <arnaudlt293@gmail.com>
2022-11-02 10:22:45 +0100

    preparing map + props

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Task from './Task';
 
 const Column = ({column, tasks}) => {
     return (
-        <div>
+        <div className='tasks-column'>
+            <h2>{column.title}</h2>
+            {tasks.map((task, index) => (
+                <Task key={task.id} task={task} index={index}/>
+            ))}
         </div>
     );
 };
