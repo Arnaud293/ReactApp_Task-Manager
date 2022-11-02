3455e2d0f067d7a735ef5a225a741a71bd03e13b
Arnaud293 <arnaudlt293@gmail.com>
2022-11-02 10:42:09 +0100

    add DragDrop content

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
 import initialData from '../config/data';
 import Column from './Column';
 
@@ -6,15 +7,20 @@ const Tasks = () => {
 
     const [data, setData] = useState(initialData);
 
+    const onDragEnd = () => {
+        
+    }
+
     return (
         <div className='tasks'>
-
+            <DragDropContext onDragEnd={onDragEnd}>
             {data.columnsOrder.map(columnId =>{
                 const column = data.columns[columnId];
                 const tasks = column.taskIds.map(taskId => data.tasks[taskId]) 
 
                 return <Column key={column.id} column={column} tasks={tasks}/>
             })}
+            </DragDropContext>
 
             
         </div>
